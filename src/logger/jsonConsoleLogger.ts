7e6313b3afccd174b57956fff9d6ebfbcec63725
jkharrat <jaouher.kharrat@eqs.com>
2019-08-27 00:14:47 +0200

    Add chalk to the logger

--- src/logger/jsonConsoleLogger.ts
+++ src/logger/jsonConsoleLogger.ts
@@ -1,26 +1,26 @@
 import {LoggerInterface} from "./loggerInterface";
+const chalk = require('chalk');
 
 class JsonConsoleLogger implements LoggerInterface {
-    // console log color referneces
-    private successOutput = "\x1b[32m";
-    private warningOutput = "\x1b[33m";
-    private dangerOutput = "\x1b[31m";
 
 
     log(data: any): void {
-        console.log(this.successOutput, JSON.stringify(data));
+        const success = chalk.bold.green;
+        console.log(success(JSON.stringify(data)));
     }
 
     logError(data: any): void {
-        console.log(this.warningOutput, JSON.stringify(data, [
+        const error = chalk.keyword('orange');
+        console.log(error(JSON.stringify(data, [
             "message", "arguments", "type", "name", "stack", "destination", "body", "parent"
-        ]));
+        ])));
     }
 
     logSecurity(data: any): void {
         // can implement here security measures like send email or...
         // currently it acts as logger only
-        console.log(this.dangerOutput, JSON.stringify(data, ["message", "arguments", "type", "name", "stack", "destination", "body"]));
+        const security = chalk.bold.red;
+        console.log(security(JSON.stringify(data, ["message", "arguments", "type", "name", "stack", "destination", "body"])));
     }
 }
 
